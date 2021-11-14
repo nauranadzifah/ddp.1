6a7c64487a9a46df437a3ebd8253265b74bbdddb
nauranadzifah <92413782+nauranadzifah@users.noreply.github.com>
2021-11-14 08:32:22 +0700

    revisi tugas1 ddp

--- simple_program.py
+++ simple_program.py
@@ -1,11 +1,11 @@
-nama = input('Nama Lengkap: ')
-dd = input('Tanggal lahir (DD): ')
-mm = input('Nama Bulan: ')
-yy = input('yyyy: ')
+nama = input('1.  Nama Lengkap: ')
+dd = input('2.  Tanggal lahir (DD): ')
+mm = input('    Nama Bulan: ')
+yy = input('    yyyy: ')
 if 2021 - int(yy) < 17:
     print('Anda terlalu muda untuk menjadi anggota')
     
-gaji = eval(input('Penghasilan per Bulan: '))
+gaji = eval(input('3.  Penghasilan per Bulan: '))
 if gaji <= 5000000:
     level = 'Silver'
 elif gaji <= 10000000:
@@ -13,28 +13,49 @@ elif gaji <= 10000000:
 else:
     level = 'Diamond'
     
-email = input('Masukan email Anda: ')
-S = '@'
-adaKarakterSpesial1 = False
-for email in S:
-    if email in '@':
-        adaKarakterSpesial1 = True
+while True:
+    N = input(str('4.  Masukan email Anda: '))
+    if '@gmail' and '.com' in N:
+        break
+    else:
+        print('Email anda tidak valid')
         
-adaKarakterSpesial2 = False        
-for email in S:
-    if email in 'gmail':
-        adaKarakterSpesial2 = True
-        
-adaKarakterSpesial3 = False
-for email in S:
-    if email in '.com':
-        adaKarakterSpesial3 = True
-        
-isValidEmail = adaKarakterSpesial1 and adaKarakterSpesial2 and adaKarakterSpesial3
-print(isValidEmail)
+while True :
+    pw = input(str("5.  Masukan Password Anda: "))
+    Panjang = len(pw)
 
+    karakter = False 
+    for i in pw :
+        if i in "!@#$" :
+            karakter = True 
+
+    Capslk = False 
+    for i in pw :
+        if i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" :
+            Capslk = True    
+
+    hrfkecil = False 
+    for i in pw :
+        if i in "abcdefghijklmnopqrstuvwxyz" :
+            hrfkecil = True 
+            
+    angka = False 
+    for i in pw :
+        if i in "1234567890" :
+            angka = True 
+    
+    if Panjang >= 8 and karakter and Capslk and hrfkecil and angka:
+        True 
+        pw1 = input(str('    Konfirmasi Password: '))
+        break
+    else : 
+        print("Password tidak valid. Ulangi.")
+        
+  
+print('         ')
+print('Pendaftaran Berhasil. Berikut Data Anda:')    
 print('Nama: ', str(nama))
-print('Email: ', str(email))
+print('Email: ', str(N))
 print('Level: ', str(level))
 
 
